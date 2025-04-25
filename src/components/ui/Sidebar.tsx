969a24fde669db7284b5fdfef2efe39a63fbc56f
HikmaAnwar <hikmaanwar89@gmail.com>
2025-04-25 15:42:23 +0300

    feat: update Sidebar component styles for improved visual consistency and hover effects

--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -10,23 +10,26 @@ export default async function Sidebar() {
         <Text className="text-primary font-bold text-xl mb-4">
           Popular This Week
         </Text>
-        <Divider className="mb-4 border-t-2 border-gray-300 visible-divider" />
+        <Divider className="mb-4 border-t border-gray-300 visible-divider" />
 
         {articles.map((article, index) => (
           <div key={article.slug}>
             {index > 0 && (
-              <Divider className="my-4 border-t-2 border-gray-300 visible-divider" />
+              <Divider className="my-4 border-t border-gray-300 visible-divider" />
             )}
-            <a href={`/articles/${article.slug}`} className="no-underline">
-              <Text className="text-white font-semibold hover:text-primary cursor-pointer transition-colors duration-200">
+            <a
+              href={`/articles/${article.slug}`}
+              className="no-underline block hover:bg-surface hover:px-2 hover:py-1 hover:rounded-md transition-all duration-200"
+            >
+              <Text className="text-white font-semibold hover:text-red-600 hover:underline cursor-pointer transition-colors duration-200">
                 {article.title}
               </Text>
-              <Group className="text-muted mb-6 inline-flex flex-row gap-4">
+              <Group className="text-muted mb-6 inline-flex flex-row gap-4 hover:text-red-600">
                 <Text
                   className={`whitespace-nowrap px-1 py-0 rounded ${
                     article.category === "Economy"
-                      ? "text-blue-600"
-                      : "text-green-500"
+                      ? "text-blue-600 hover:text-red-600"
+                      : "text-green-500 hover:text-red-600"
                   }`}
                 >
                   {article.category}
