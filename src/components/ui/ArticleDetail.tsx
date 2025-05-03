271422444668f4b01548db20f9aef113aa72235b
HikmaAnwar <hikmaanwar89@gmail.com>
2025-05-03 14:32:24 +0300

    Refactor code structure for improved readability and maintainability

--- src/components/ui/ArticleDetail.tsx
+++ src/components/ui/ArticleDetail.tsx
@@ -1,7 +1,8 @@
-import { Card, Title, Text, Image, Group } from "@mantine/core";
+import { Title, Text, Image, Group } from "@mantine/core";
 import Sidebar from "@/components/ui/Sidebar";
 import { SocialShare } from "@/components/ui/SocialShare";
 import { RelatedArticles } from "@/components/ui/RelatedArticles";
+import { InlineCTA } from "./InlineCTA";
 
 interface Article {
   title: string;
@@ -26,7 +27,7 @@ export function ArticleDetail({ article }: ArticleDetailProps) {
     author: "John Abebe",
     date: "May 15, 2025",
     readTime: "8 min read",
-    image: "/images/gerd-placeholder.jpg",
+    image: "/images/frehiwot.jpg",
     content: (
       <>
         <Text className="mb-4 text-primary">
@@ -64,67 +65,70 @@ export function ArticleDetail({ article }: ArticleDetailProps) {
     <section className="w-full py-12">
       <div className="flex flex-col sm:flex-row items-start space-x-0 sm:space-x-8">
         <div className="w-full sm:flex-1 sm:max-w-[680px] min-w-0">
-          <Card className="bg-background border border-border shadow-md rounded-2xl p-6 mb-8">
-            <Title
-              order={1}
-              className="text-blueblack-white font-serif mb-4 text-5xl font-bold whitespace-normal tracking-wide leading-tight"
-            >
-              {displayArticle.title}
-            </Title>
-            <Group className="text-muted font-serif mb-6 inline-flex flex-row gap-4">
-              <Text className="whitespace-nowrap text-red-600 bg-red-900 px-1 py-0 rounded">
-                {displayArticle.category}
-              </Text>
-              <Text className="whitespace-nowrap">
-                By {displayArticle.author}
-              </Text>
-              <Text className="whitespace-nowrap">{displayArticle.date}</Text>
-              <Text className="whitespace-nowrap">
-                {displayArticle.readTime}
-              </Text>
-            </Group>
-            <SocialShare />
-            <div className="relative my-6">
+          <Title
+            order={1}
+            className="text-blueblack-white font-serif mb-4 text-5xl font-bold whitespace-normal tracking-wide leading-tight"
+          >
+            {displayArticle.title}
+          </Title>
+
+          <Group className="text-muted font-serif mb-6 inline-flex flex-row gap-4">
+            <Text className="whitespace-nowrap text-red-600 bg-red-900 px-1 py-0 rounded">
+              {displayArticle.category}
+            </Text>
+            <Text className="whitespace-nowrap">
+              By {displayArticle.author}
+            </Text>
+            <Text className="whitespace-nowrap">{displayArticle.date}</Text>
+            <Text className="whitespace-nowrap">{displayArticle.readTime}</Text>
+          </Group>
+
+          <SocialShare />
+
+          <div className="relative my-6">
+            <Image
+              src={displayArticle.image}
+              alt="Grand Ethiopian Renaissance Dam"
+              className="rounded-lg"
+              height="400"
+              width="680"
+            />
+            <div className="absolute top-7 left-7">
               <Image
-                src={displayArticle.image}
-                alt="Grand Ethiopian Renaissance Dam"
-                className="rounded-lg"
-                height="400"
-                width="680"
+                src="/images/logo-red.png"
+                alt="Ensight Logo"
+                className="h-8"
               />
-              <div className="absolute top-7 left-7">
-                <Image
-                  src="/images/logo-red.png"
-                  alt="Ensight Logo"
-                  className="h-8"
-                />
-              </div>
-              <div
-                className="absolute bottom-0 left-0 w-full h-1/3"
-                style={{
-                  background:
-                    "linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 1))",
-                }}
-              />
-              <div
-                className="absolute top-0 left-0 w-full h-1/3"
-                style={{
-                  background:
-                    "linear-gradient(to top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.7))",
-                }}
-              />
-              <div className="absolute bottom-4 left-4">
-                <Text
-                  className="text-white font-serif text-[28px] font-extrabold"
-                  style={{ textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)" }}
-                >
-                  {displayArticle.title}
-                </Text>
-              </div>
             </div>
+            <div
+              className="absolute bottom-0 left-0 w-full h-1/3"
+              style={{
+                background:
+                  "linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 1))",
+              }}
+            />
+            {/* <div
+              className="absolute top-0 left-0 w-full h-1/3"
+              style={{
+                background:
+                  "linear-gradient(to top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.7))",
+              }}
+            /> */}
+            <div className="absolute bottom-4 left-4">
+              <Text
+                className="text-white font-serif text-[28px] font-extrabold"
+                style={{ textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)" }}
+              >
+                {displayArticle.title}
+              </Text>
+            </div>
+          </div>
+
+          <div className="prose prose-lg prose-invert font-serif text-primary max-w-none">
             {displayArticle.content}
-          </Card>
+          </div>
         </div>
+
         <div className="w-full sm:w-[480px] mt-8 sm:mt-0 sm:ml-auto">
           <Sidebar />
           <RelatedArticles />
