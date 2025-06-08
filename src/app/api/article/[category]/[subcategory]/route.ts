a03c696a94746642923ae18d24828c70285204bb
Hikma Anwar <114664132+HikmaAnwar@users.noreply.github.com>
2025-06-08 21:26:25 +0300

    Update route.ts

--- src/app/api/article/[category]/[subcategory]/route.ts
+++ src/app/api/article/[category]/[subcategory]/route.ts
@@ -1,21 +1,18 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { BASE_URL } from "@/lib/constants";
 
-export async function GET({
-  params,
-}: {
-  params: Promise<{ category: string; subcategory: string }>;
-}) {
-  const resolvedParams = await params;
-  const { category, subcategory } = resolvedParams;
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { category: string; subcategory: string } }
+) {
+  const { category, subcategory } = params;
 
-  // const token = req.headers.get("authorization");
   try {
     const response = await fetch(
       `${BASE_URL}/article/${category}/${subcategory}`,
       {
         headers: {
-          // Authorization: token || "",
+          // Authorization: req.headers.get("authorization") || "",
         },
       }
     );
@@ -30,7 +27,6 @@ export async function GET({
 
     const data = await response.json();
     return NextResponse.json(data, { status: 200 });
-    //eslint-disable-next-line
   } catch (error: any) {
     console.error("Fetch error:", error);
     return NextResponse.json(
