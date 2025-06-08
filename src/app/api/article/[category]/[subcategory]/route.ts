e777bb6e01aefccc0719967944f7b6cd05add7b9
Hikma Anwar <114664132+HikmaAnwar@users.noreply.github.com>
2025-06-08 21:46:06 +0300

    Update route.ts

--- src/app/api/article/[category]/[subcategory]/route.ts
+++ src/app/api/article/[category]/[subcategory]/route.ts
@@ -1,9 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
+import type { RouteHandlerContext } from "next/dist/server/web/types"; // <- Fix here
 import { BASE_URL } from "@/lib/constants";
 
 export async function GET(
   req: NextRequest,
-  context: { params: { category: string; subcategory: string } }
+  context: RouteHandlerContext<{ category: string; subcategory: string }>
 ) {
   const { category, subcategory } = context.params;
 
