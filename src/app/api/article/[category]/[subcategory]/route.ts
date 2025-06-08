348d63296e6e692f31830289cd76464c57444eca
Hikma Anwar <114664132+HikmaAnwar@users.noreply.github.com>
2025-06-08 21:34:47 +0300

    Update route.ts

--- src/app/api/article/[category]/[subcategory]/route.ts
+++ src/app/api/article/[category]/[subcategory]/route.ts
@@ -28,7 +28,6 @@ export async function GET(
     const data = await response.json();
     return NextResponse.json(data, { status: 200 });
     //eslint-disable-next-line
-
   } catch (error: any) {
     console.error("Fetch error:", error);
     return NextResponse.json(
