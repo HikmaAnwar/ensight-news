2caaf9bde52b3933c915d18e66666397e90f3ecb
Hikma Anwar <114664132+HikmaAnwar@users.noreply.github.com>
2025-07-15 22:15:51 +0300

    Delete src directory

--- src/lib/theme.ts
+++
@@ -1,7 +0,0 @@
-// lib/theme.ts
-import { useMantineColorScheme } from "@mantine/core";
-
-export function useTheme() {
-  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
-  return { colorScheme, toggleColorScheme };
-}
